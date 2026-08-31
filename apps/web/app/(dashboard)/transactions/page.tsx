7d311aade66f5960a4aab3d531ee16f907ef831f
John William <johnwilliams1756@gmail.com>
2026-08-31 20:18:11 +0300

    fix spellings

--- apps/web/app/(dashboard)/transactions/page.tsx
+++ apps/web/app/(dashboard)/transactions/page.tsx
@@ -1,13 +1,13 @@
 import { TransactionSummary } from "../../../features/transactions/components/TransactionSummary";
 import { TransactionHeader } from "../../../features/transactions/components/TransactionHeader";
-import { TransactionsWorkspace } from "../../../features/transactions/components/TransactionWorkspace";
+import { TransactionWorkspace } from "../../../features/transactions/components/TransactionWorkspace";
 
 export default function TransactionPage() {
     return (
         <main className="min-w-0">
             <TransactionHeader/>
             <TransactionSummary/>
-            <TransactionsWorkspace/>
+            <TransactionWorkspace/>
         </main>
     )
 }
